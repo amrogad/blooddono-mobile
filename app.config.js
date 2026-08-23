2b0dca5b2131e6502edc64e26ec380ffc871bacf
Amro Gad <amrelsayedrabie@gmail.com>
2026-08-23 15:51:29 +0300

    bump version to 1.1.0

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: 'BloodDono',
     slug: 'blooddono-mobile',
-    version: '1.0.0',
+    version: '1.1.0',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
     scheme: 'blooddono',
